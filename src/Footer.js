24e8f72fbc05f535df76966d7a63d508567252bb
Lindsay Block <lindsay.block05@gmail.com>
2018-11-14 10:24:33 -0700

    initial commit

---
+++ src/Footer.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Container, Button, Menu, Segment, Divider, Header} from 'semantic-ui-react';
+
+class Footer extends Component {
+
+  render () {
+    return (
+      <div>
+        <Segment  vertical style={{ alignItems: "center", padding: '2em', backgroundColor: "rgb(246, 245, 245)"}}>
+         <Container textAlign="center">
+             <a href="https://www.facebook.com/lindsay.wolthuis">
+               <Button size="huge" circular color='facebook' icon='facebook' />
+             </a>
+               <a href="https://www.linkedin.com/in/lindsay-block0805">
+               <Button size="huge" circular color='linkedin' icon='linkedin' />
+             </a>
+             <a href="https://github.com/lindsblock">
+               <Button size="huge" circular color="black" icon='github' />
+             </a>
+            <Menu secondary widths={6}>
+                <a  style={{padding:'10px'}} href="/about">
+                <Header style={styles.navHeaders} as="h3">About</Header>
+              </a>
+              <a style={{padding:'10px'}}  href="/projects">
+                <Header style={styles.navHeaders} as="h3">Projects</Header>
+              </a>
+              <a style={{padding:'10px'}}  href="/contact">
+                <Header style={styles.navHeaders} as="h3">Contact</Header>
+              </a>
+            </Menu>
+         </Container>
+       </Segment>
+      </div>
+    )
+
+  }
+}
+const styles = {
+  pageHeaders: {
+    fontFamily: 'Tangerine',
+    fontSize: '100px',
+    marginRight: '20px',
+    paddingTop: '10px',
+  },
+  navHeaders: {
+    fontFamily: 'Marcellus',
+    fontSize: '20px',
+    backgroundColor: 'rgb(246, 245, 245)',
+    color:'rgb(114, 166, 96)'
+  }
+}
+
+export default Footer;
