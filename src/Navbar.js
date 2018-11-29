b8560d7d33a0475791b9b02be957c76247c63c6e
Lindsay Block <lindsay.block05@gmail.com>
2018-11-29 12:45:08 -0700

    styling mobile navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Dropdown, Menu } from 'semantic-ui-react';
 import './Navbar.css';
 
 const navbar = () => (
@@ -25,8 +26,18 @@ const navbar = () => (
         </ul>
       </div>
     </nav>
+    <div className="mobileSizeDiv">
+      <Dropdown floating text='Menu' style={{color: 'rgb(99,99,99)', fontFamily:'marcellus',fontSize: '24px', margin: '20px'}}>
+        <Dropdown.Menu style={{background:'rgb(246, 245, 245)'}}>
+          <Dropdown.Item style={{color:'rgb(99,99,99)'}} as="a" href="/" text='Home' />
+          <Dropdown.Item style={{color:'rgb(99,99,99)'}} as="a" href="/about" text='About' />
+          <Dropdown.Item style={{color:'rgb(99,99,99)'}} as="a" href="projects" text='Projects'  />
+          <Dropdown.Item style={{color:'rgb(99,99,99)'}} as="a" href="contact" text='Contact' />
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
   </header>
-  )
+)
 
 
 export default navbar;
