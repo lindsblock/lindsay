f5dfab9ab4758ed3581521cf59065e06cac1dccf
Lindsay Block <lindsay.block05@gmail.com>
2018-12-12 11:31:11 -0700

    adding css page to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import { Image, Button } from 'semantic-ui-react';
+import './Home.css';
 
 class Home extends Component {
   render(){
     return(
       <div>
         <Image style={{padding: '20px', marginBottom:'50px', marginTop:'50px'}} src="../images/logo.png" size="massive" centered/>
-        <div style={{
-          // margin: '0',
-          // padding: '70px',
-          textAlign: 'center',
-          fontSize: '20px',
-          fontFamily: 'marcellus',
-          background: 'rgb(246, 245, 245, .9)',
-        }}>
+        <div className="home">
           <p style={{fontSize:'30px', color:'rgb(99,99,99)'}}>Looking for a Web Developer?</p>
           <p style={{fontSize:'20px', color:'rgb(99,99,99)'}}>Look no further! My name is Lindsay Block, and I am a Full-Stack Web Developer based in SLC, Utah. </p>
           <p style={{fontSize:'20px', color:'rgb(99,99,99)'}}>I can build you a fully responsive, custom designed website specific to your needs.</p>
